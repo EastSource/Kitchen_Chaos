a0d90993cb46f4e644ba8e2825267816e63ef790
EastSource <139376938+EastSource@users.noreply.github.com>
2024-12-08 00:07:14 +0900

    to Interface

--- Assets/Script/ClearCounter.cs
+++ Assets/Script/ClearCounter.cs
@@ -2,10 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
+public class ClearCounter : MonoBehaviour, IKitchenObjectParent
 {
-    public void Interact()
+    [SerializeField]private KitchinObjectSO kitchinObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    
+    private KitchinObject kitchenObject;
+    
+    public void Interact(Player player)
     {
-        Debug.Log("Interact!");
+        
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchinObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchinObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            // Give the object to player
+             kitchenObject.SetKitchenObjectParent(player);
+            Debug.Log(kitchenObject.GetKicthenObjectParent());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchinObject kitchinObject)
+    {
+        this.kitchenObject = kitchinObject;
+    }
+
+    public KitchinObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Script/GameInput.cs
+++ Assets/Script/GameInput.cs
@@ -2,15 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractionAction;
     private PlayerInputAction playerInputAction;
     private void Awake()
     {
         playerInputAction = new PlayerInputAction();
         playerInputAction.Player.Enable();
+
+        playerInputAction.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    { 
+        OnInteractionAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
