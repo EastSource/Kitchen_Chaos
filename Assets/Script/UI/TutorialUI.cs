acf7e2b8c52c481819eb09a259f000f09038ed70
EastSource <139376938+EastSource@users.noreply.github.com>
2024-12-22 04:04:27 +0900

    Polish-01

---
+++ Assets/Script/UI/TutorialUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI keyMoveUpText;
+    [SerializeField] private TextMeshProUGUI keyMoveDownText;
+    [SerializeField] private TextMeshProUGUI keyMoveLeftText;
+    [SerializeField] private TextMeshProUGUI keyMoveRightText;
+    [SerializeField] private TextMeshProUGUI keyMoveInteractText;
+    [SerializeField] private TextMeshProUGUI keyMoveInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI KeyMovePauseText;
+    [SerializeField] private TextMeshProUGUI keyGamePadMove;
+    [SerializeField] private TextMeshProUGUI KeyMoveGamePadInteractText;
+    [SerializeField] private TextMeshProUGUI KeyMoveGamePadInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI KeyMoveGamePadPauseText;
+    
+}
