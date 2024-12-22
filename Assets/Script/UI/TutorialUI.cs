5f2cd7df25ca1e2aa365abbeba0b98f6eab0602b
EastSource <139376938+EastSource@users.noreply.github.com>
2024-12-23 01:17:52 +0900

    Finish

--- Assets/Script/UI/TutorialUI.cs
+++ Assets/Script/UI/TutorialUI.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class TutorialUI : MonoBehaviour
 {
@@ -16,5 +19,46 @@ public class TutorialUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI KeyMoveGamePadInteractText;
     [SerializeField] private TextMeshProUGUI KeyMoveGamePadInteractAlternateText;
     [SerializeField] private TextMeshProUGUI KeyMoveGamePadPauseText;
-    
+
+    private void Start()
+    {
+        GameInput.Instance.OnBindingRebind += GameInput_OnBindingRebind;
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+        UpdateVisual();
+        Show();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, EventArgs e)
+    {
+        if (KitchenGameManager.Instance.IsCountdownToStartActive())
+        {
+            Hide();
+        }
+    }
+
+    private void GameInput_OnBindingRebind(object sender, EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        keyMoveUpText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Up);
+        keyMoveDownText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Down);
+        keyMoveLeftText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Left);
+        keyMoveRightText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Right);
+        keyMoveInteractText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
+        keyMoveInteractAlternateText.text = GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlternate);
+        KeyMovePauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
 }
